b7a70f15fa226eeaf356b1a3598c3301acd98155
Benjamin Kroll <benk@mugo.ca>
2026-06-10 13:05:32 +1000

    Added urlalias:* commands (create, list, refresh, clean-up)

---
+++ src/Command/EepUrlAliasCreateCommand.php
@@ -0,0 +1,209 @@
+<?php
+
+namespace MugoWeb\Eep\Bundle\Command;
+
+use MugoWeb\Eep\Bundle\Services\EepLogger;
+use MugoWeb\Eep\Bundle\Component\Console\Helper\Table;
+use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\PermissionResolver;
+use eZ\Publish\API\Repository\UserService;
+use eZ\Publish\API\Repository\URLAliasService;
+use eZ\Publish\API\Repository\Exceptions;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class EepUrlAliasCreateCommand extends Command
+{
+    public function __construct
+    (
+        URLAliasService $urlAliasService,
+        LocationService $locationService,
+        PermissionResolver $permissionResolver,
+        UserService $userService,
+        EepLogger $logger
+    )
+    {
+        $this->urlAliasService = $urlAliasService;
+        $this->locationService = $locationService;
+        $this->permissionResolver = $permissionResolver;
+        $this->userService = $userService;
+        $this->logger = $logger;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $help = <<<EOD
+<info>Usage</info>
+-----
+eep:urlalias:create 43 '/my-alias' eng-GB
+
+eep:urlalias:create 43 '/my-alias' eng-GB --forwarding --always-available
+
+EOD;
+
+        $this
+            ->setName('eep:urlalias:create')
+            ->setAliases(array('eep:ua:create'))
+            ->setDescription('Creates a custom URL alias for a location')
+            ->addArgument('location-id', InputArgument::REQUIRED, 'Location id')
+            ->addArgument('path', InputArgument::REQUIRED, 'URL alias path')
+            ->addArgument('language-code', InputArgument::REQUIRED, 'Language code')
+            ->addOption('forwarding', 'f', InputOption::VALUE_NONE, 'Perform redirect from the alias')
+            ->addOption('always-available', 'a', InputOption::VALUE_NONE, 'Make alias available in all languages')
+            ->addOption('result-format', 'r', InputOption::VALUE_OPTIONAL, 'Result display format. One of: default, table, minimal', 'default')
+            ->addOption('no-newline', 'x', InputOption::VALUE_NONE, 'Result display without trailing newline. Only applies when --result-format=minimal')
+            ->addOption('user-id', 'u', InputOption::VALUE_OPTIONAL, 'User id for URL alias operations', 14)
+            ->setHelp($help)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $inputLocationId = $input->getArgument('location-id');
+        $inputPath = $input->getArgument('path');
+        $inputLanguageCode = $input->getArgument('language-code');
+        $inputForwarding = $input->getOption('forwarding');
+        $inputAlwaysAvailable = $input->getOption('always-available');
+        $inputUserId = $input->getOption('user-id');
+
+        $this->permissionResolver->setCurrentUserReference($this->userService->loadUser($inputUserId));
+
+        $location = $this->locationService->loadLocation($inputLocationId);
+
+        $io = new SymfonyStyle($input, $output);
+        $confirm = $input->getOption('no-interaction');
+        if (!$confirm)
+        {
+            $confirm = $io->confirm(
+                sprintf(
+                    'Are you sure you want to create URL alias "%s" at "%s"?',
+                    $inputPath,
+                    $location->contentInfo->name
+                ),
+                false
+            );
+        }
+
+        if ($confirm)
+        {
+            $loggerContext = array
+            (
+                $inputLocationId,
+                $inputPath,
+                $inputLanguageCode,
+                (integer) $inputForwarding,
+                (integer) $inputAlwaysAvailable,
+                $inputUserId
+            );
+            $this->logger->info($this->getName() . " confirmed", $loggerContext);
+
+            try
+            {
+                $urlAlias = $this->urlAliasService->createUrlAlias
+                (
+                    $location,
+                    $inputPath,
+                    $inputLanguageCode,
+                    $inputForwarding,
+                    $inputAlwaysAvailable
+                );
+
+                switch ($input->getOption('result-format'))
+                {
+                    case 'table':
+                        {
+                            $rows = array
+                            (
+                                array
+                                (
+                                    $urlAlias->id,
+                                    $urlAlias->type,
+                                    $urlAlias->destination,
+                                    $urlAlias->path,
+                                    implode(',', $urlAlias->languageCodes),
+                                    (integer) $urlAlias->alwaysAvailable,
+                                    (integer) $urlAlias->isHistory,
+                                    (integer) $urlAlias->isCustom,
+                                    (integer) $urlAlias->forward,
+                                )
+                            );
+                            $headers = array
+                            (
+                                array
+                                (
+                                    'id',
+                                    'type',
+                                    'destination',
+                                    'path',
+                                    'languageCodes',
+                                    'alwaysAvailable',
+                                    'isHistory',
+                                    'isCustom',
+                                    'forward',
+                                )
+                            );
+                            $infoHeader = array
+                            (
+                                new TableCell
+                                (
+                                    "{$this->getName()} [$inputLocationId $inputPath $inputLanguageCode]",
+                                    array('colspan' => count($headers[0]))
+                                )
+                            );
+                            array_unshift($headers, $infoHeader);
+
+                            $table = new Table($output);
+                            $table->setHeaders($headers);
+                            $table->setRows($rows);
+                            $table->render();
+                        }
+                        break;
+
+                    case 'minimal':
+                        {
+                            if ($input->getOption('no-newline'))
+                            {
+                                $io->write("{$urlAlias->id} {$urlAlias->path}");
+                            }
+                            else
+                            {
+                                $io->writeln("{$urlAlias->id} {$urlAlias->path}");
+                            }
+                        }
+                        break;
+
+                    default:
+                    {
+                        $io->success("Create successful. urlAliasId: {$urlAlias->id} path: {$urlAlias->path}");
+                    }
+                }
+
+                $this->logger->info($this->getName() . " successful", array($urlAlias->id, $urlAlias->path));
+            }
+            catch
+            (
+                Exceptions\InvalidArgumentException |
+                Exceptions\UnauthorizedException |
+                Exceptions\NotFoundException
+                $e
+            )
+            {
+                $io->error($e->getMessage());
+                $this->logger->error($this->getName() . " error", array($e->getMessage()));
+            }
+        }
+        else
+        {
+            $io->writeln('Create cancelled by user action');
+        }
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Command/EepUrlAliasLookupCommand.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace MugoWeb\Eep\Bundle\Command;
+
+use MugoWeb\Eep\Bundle\Services\EepLogger;
+use MugoWeb\Eep\Bundle\Component\Console\Helper\Table;
+use eZ\Publish\API\Repository\PermissionResolver;
+use eZ\Publish\API\Repository\UserService;
+use eZ\Publish\API\Repository\URLAliasService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class EepUrlAliasLookupCommand extends Command
+{
+    public function __construct
+    (
+        URLAliasService $urlAliasService,
+        PermissionResolver $permissionResolver,
+        UserService $userService,
+        EepLogger $logger
+    )
+    {
+        $this->urlAliasService = $urlAliasService;
+        $this->permissionResolver = $permissionResolver;
+        $this->userService = $userService;
+        $this->logger = $logger;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $help = <<<EOD
+TODO
+
+EOD;
+
+        $this
+            ->setName('eep:urlalias:info')
+            ->setAliases(array('eep:ua:info'))
+            ->setDescription('Returns URL alias by URL')
+            ->addArgument('url', InputArgument::REQUIRED, 'URL')
+            ->addOption('language', 'l', InputOption::VALUE_OPTIONAL, 'Language code')
+            ->addOption('user-id', 'u', InputOption::VALUE_OPTIONAL, 'User id for URL alias operations', 14)
+            ->setHelp($help)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $inputUrl = $input->getArgument('url');
+        $inputLanguage = $input->getOption('language');
+        $inputUserId = $input->getOption('user-id');
+
+        $this->permissionResolver->setCurrentUserReference($this->userService->loadUser($inputUserId));
+
+        $urlAlias = $this->urlAliasService->lookup($inputUrl, $inputLanguage);
+
+        $headers = array
+        (
+            array
+            (
+                'key',
+                'value',
+            ),
+        );
+        $colWidth = count($headers[0]);
+        $infoHeader = array
+        (
+            new TableCell("{$this->getName()} [$inputUrl]", array('colspan' => $colWidth))
+        );
+        array_unshift($headers, $infoHeader);
+
+        $rows = array
+        (
+            array('id', $urlAlias->id),
+            array('type', $urlAlias->type),
+            array('destination', $urlAlias->destination),
+            array('path', $urlAlias->path),
+            array('languageCodes', implode(',', $urlAlias->languageCodes)),
+            array('alwaysAvailable', (integer) $urlAlias->alwaysAvailable),
+            array('isHistory', (integer) $urlAlias->isHistory),
+            array('isCustom', (integer) $urlAlias->isCustom),
+            array('forward', (integer) $urlAlias->forward),
+        );
+
+        $io = new SymfonyStyle($input, $output);
+        $table = new Table($output);
+        $table->setHeaders($headers);
+        $table->setRows($rows);
+        $table->render();
+        $io->newLine();
+
+        return Command::SUCCESS;
+    }
+}
