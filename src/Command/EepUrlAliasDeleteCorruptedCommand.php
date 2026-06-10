ffed9d6f0d294df77ecb378225ee9e3b58052fd7
Benjamin Kroll <benk@mugo.ca>
2026-06-10 13:04:36 +1000

    Added urlalias:* commands (create, list, refresh, clean-up)

---
+++ src/Command/EepUrlAliasDeleteCorruptedCommand.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace MugoWeb\Eep\Bundle\Command;
+
+use MugoWeb\Eep\Bundle\Services\EepLogger;
+use Ibexa\Contracts\Core\Repository\PermissionResolver;
+use Ibexa\Contracts\Core\Repository\UserService;
+use Ibexa\Contracts\Core\Repository\URLAliasService;
+use Ibexa\Contracts\Core\Repository\Exceptions;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class EepUrlAliasDeleteCorruptedCommand extends Command
+{
+    public function __construct
+    (
+        URLAliasService $urlAliasService,
+        PermissionResolver $permissionResolver,
+        UserService $userService,
+        EepLogger $logger
+    )
+    {
+        $this->urlAliasService = $urlAliasService;
+        $this->permissionResolver = $permissionResolver;
+        $this->userService = $userService;
+        $this->logger = $logger;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $help = <<<EOD
+TODO
+
+EOD;
+
+        $this
+            ->setName('eep:urlalias:deletecorrupted')
+            ->setAliases(array('eep:ua:deletecorrupted'))
+            ->setDescription('Deletes corrupted URL aliases')
+            ->addOption('user-id', 'u', InputOption::VALUE_OPTIONAL, 'User id for URL alias operations', 14)
+            ->setHelp($help)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $inputUserId = $input->getOption('user-id');
+
+        $this->permissionResolver->setCurrentUserReference($this->userService->loadUser($inputUserId));
+
+        $io = new SymfonyStyle($input, $output);
+        $confirm = $input->getOption('no-interaction');
+        if (!$confirm)
+        {
+            $confirm = $io->confirm(
+                'Are you sure you want to delete corrupted URL aliases?',
+                false
+            );
+        }
+
+        if ($confirm)
+        {
+            $loggerContext = array
+            (
+                $inputUserId
+            );
+            $this->logger->info($this->getName() . " confirmed", $loggerContext);
+
+            try
+            {
+                $deletedCount = $this->urlAliasService->deleteCorruptedUrlAliases();
+
+                $io->success(sprintf('Delete successful. deletedCount: %d', $deletedCount));
+                $this->logger->info($this->getName() . " successful", array($deletedCount));
+            }
+            catch
+            (
+                Exceptions\InvalidArgumentException |
+                Exceptions\UnauthorizedException
+                $e
+            )
+            {
+                $io->error($e->getMessage());
+                $this->logger->error($this->getName() . " error", array($e->getMessage()));
+            }
+        }
+        else
+        {
+            $io->writeln('Delete cancelled by user action');
+        }
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Command/EepUrlAliasRefreshSystemCommand.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace MugoWeb\Eep\Bundle\Command;
+
+use MugoWeb\Eep\Bundle\Services\EepLogger;
+use Ibexa\Contracts\Core\Repository\LocationService;
+use Ibexa\Contracts\Core\Repository\PermissionResolver;
+use Ibexa\Contracts\Core\Repository\UserService;
+use Ibexa\Contracts\Core\Repository\URLAliasService;
+use Ibexa\Contracts\Core\Repository\Exceptions;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class EepUrlAliasRefreshSystemCommand extends Command
+{
+    public function __construct
+    (
+        URLAliasService $urlAliasService,
+        LocationService $locationService,
+        PermissionResolver $permissionResolver,
+        UserService $userService,
+        EepLogger $logger
+    )
+    {
+        $this->urlAliasService = $urlAliasService;
+        $this->locationService = $locationService;
+        $this->permissionResolver = $permissionResolver;
+        $this->userService = $userService;
+        $this->logger = $logger;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $help = <<<EOD
+TODO
+
+EOD;
+
+        $this
+            ->setName('eep:urlalias:refreshsystem')
+            ->setAliases(array('eep:ua:refreshsystem'))
+            ->setDescription('Refreshes system URL aliases for a location')
+            ->addArgument('location-id', InputArgument::REQUIRED, 'Location id')
+            ->addOption('user-id', 'u', InputOption::VALUE_OPTIONAL, 'User id for URL alias operations', 14)
+            ->setHelp($help)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $inputLocationId = $input->getArgument('location-id');
+        $inputUserId = $input->getOption('user-id');
+
+        $this->permissionResolver->setCurrentUserReference($this->userService->loadUser($inputUserId));
+
+        $location = $this->locationService->loadLocation($inputLocationId);
+
+        $io = new SymfonyStyle($input, $output);
+        $confirm = $input->getOption('no-interaction');
+        if (!$confirm)
+        {
+            $confirm = $io->confirm(
+                sprintf(
+                    'Are you sure you want to refresh system URL aliases for "%s"?',
+                    $location->contentInfo->name
+                ),
+                false
+            );
+        }
+
+        if ($confirm)
+        {
+            $loggerContext = array
+            (
+                $inputLocationId,
+                $inputUserId
+            );
+            $this->logger->info($this->getName() . " confirmed", $loggerContext);
+
+            try
+            {
+                $this->urlAliasService->refreshSystemUrlAliasesForLocation($location);
+
+                $io->success(sprintf('Refresh successful. locationId: %s', $inputLocationId));
+                $this->logger->info($this->getName() . " successful", array($inputLocationId));
+            }
+            catch
+            (
+                Exceptions\InvalidArgumentException |
+                Exceptions\UnauthorizedException |
+                Exceptions\NotFoundException
+                $e
+            )
+            {
+                $io->error($e->getMessage());
+                $this->logger->error($this->getName() . " error", array($e->getMessage()));
+            }
+        }
+        else
+        {
+            $io->writeln('Refresh cancelled by user action');
+        }
+
+        return Command::SUCCESS;
+    }
+}
