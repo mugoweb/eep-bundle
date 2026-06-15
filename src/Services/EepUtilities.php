9c0c624eb81d77d3c2b4562f3269e9c9a858ed11
Benjamin Kroll <benk@mugo.ca>
2026-06-15 13:36:13 +1000

    Utilities service updates - label helpers for sort field, sort order

--- src/Services/EepUtilities.php
+++ src/Services/EepUtilities.php
@@ -43,6 +43,43 @@ class EepUtilities
         return (isset($labels[$hashAlgorithmId]))? $labels[$hashAlgorithmId] : 'N/A';
     }
 
+    public static function getLocationSortFieldLabel($sortFieldId)
+    {
+        $labels = array
+        (
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_PATH => 'PATH',
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_PUBLISHED => 'PUBLISHED',
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_MODIFIED => 'MODIFIED',
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_SECTION => 'SECTION',
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_DEPTH => 'DEPTH',
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_CLASS_IDENTIFIER => 'CLASS_IDENTIFIER',
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_CLASS_NAME => 'CLASS_NAME',
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_PRIORITY => 'PRIORITY',
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_NAME => 'NAME',
+
+            /**
+             * @deprecated
+             */
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_MODIFIED_SUBNODE => 'MODIFIED_SUBNODE',
+
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_NODE_ID => 'NODE_ID',
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_FIELD_CONTENTOBJECT_ID => 'CONTENTOBJECT_ID',
+        );
+
+        return (isset($labels[$sortFieldId]))? $labels[$sortFieldId] : 'N/A';
+    }
+
+    public static function getLocationSortOrderLabel($sortOrderId)
+    {
+        $labels = array
+        (
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_ORDER_DESC => 'DESC',
+            \eZ\Publish\API\Repository\Values\Content\Location::SORT_ORDER_ASC => 'ASC',
+        );
+
+        return (isset($labels[$sortOrderId]))? $labels[$sortOrderId] : 'N/A';
+    }
+
     public static function stripColumnMarkers($columnIdentifier)
     {
         $s = array( ' *' );
